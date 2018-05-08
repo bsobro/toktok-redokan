12f409777e97ea700bfa608d32dac2c434ae868f
joona yoon <joonas.yoon@gmail.com>
2018-05-08 15:56:22 +0900

    ~83p) Hook (action & filter)

--- wp-content/themes/joonas/functions.php
+++ wp-content/themes/joonas/functions.php
@@ -16,3 +16,23 @@ function main_sidebar() {
 }
 
 add_action('widgets_init', 'main_sidebar');
+
+function dummy_data() {
+    ?><meta name="dummy" content="test meta"/><?php
+}
+
+add_action('wp_head','dummy_data');
+
+/**
+ * https://developer.wordpress.org/reference/hooks/wp_title/
+ *
+ * Note that the filter function *must* return the content after it is finished processing,
+ * or the title will be blank and other plugins also filtering the content may generate errors.
+ **/
+function dummy_title($title, $sep, $seplocation) {
+    $title .= ' - Dummy';
+    return $title;
+}
+
+add_filter('wp_title', 'dummy_title', 10, 3);
+
