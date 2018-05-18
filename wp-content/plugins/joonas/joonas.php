3b4691445e1678b6d640714f506c3fd798258b97
joona yoon <joonas.yoon@gmail.com>
2018-05-18 16:08:47 +0900

    클래스 폼 적용

--- wp-content/plugins/joonas/joonas.php
+++ wp-content/plugins/joonas/joonas.php
@@ -1,17 +1,39 @@
 <?php
- /*
- Plugin Name: Plugin Name is joonas
- Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
- Description: A brief description of the Plugin.
- Version: 0.1.0
- Author: Joona Yoon
- Author URI: http://URI_Of_The_Plugin_Author
- License: A "Slug" license name e.g. GPL2
+/**
+ * @package Joonas
  */
+/*
+Plugin Name: 00 Joonas Plugin
+Plugin URI: https://joonas.tistory.com/
+Description: asdfasdfasdf
+Version: 0.1.0
+Author: Joona Yoon
+Author URI: https://joonas.tistory.com/
+License: None
+Text Domain: JoonasPlugin
+*/
 
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
 
-register_activation_hook( __FILE__, array( 'JoonasPlugIn', 'plugin_activation' ) );
-register_deactivation_hook( __FILE__, array( 'JoonasPlugIn', 'plugin_deactivation' ) );
+define( 'JOONAS_VERSION', '4.0.3' );
+define( 'JOONAS__MINIMUM_WP_VERSION', '4.0' );
+define( 'JOONAS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'JOONAS_DELETE_LIMIT', 100000 );
 
+register_activation_hook( __FILE__, array( 'Joonas', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Joonas', 'deactivate' ) );
 
-?>
+require_once( JOONAS__PLUGIN_DIR . 'class.joonas.php' );
+/*
+require_once( JOONAS__PLUGIN_DIR . 'class.joonas-widget.php' );
+require_once( JOONAS__PLUGIN_DIR . 'class.joonas-rest-api.php' );
+*/
+
+add_action( 'init', array( 'Joonas', 'init' ) );
+
+// add_action( 'rest_api_init', array( 'Joonas_REST_API', 'init' ) );
+
